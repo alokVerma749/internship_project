f7721841f8aefb2045996bc6fc259c1f6c9fe736
alokverma749 <alokverma749@gmail.com>
2024-07-09 20:17:01 +0530

    try

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import './App.css';
 
 function App() {
@@ -6,6 +7,29 @@ function App() {
     const status = await window.FB.getLoginStatus()
     console.log(status, '###')
   }
+
+  useEffect(() => {
+    // Initialize Facebook SDK
+    window.fbAsyncInit = function() {
+      window.FB.init({
+        appId      : '762762392464716',
+        cookie     : true,
+        xfbml      : true,
+        version    : 'v20.0'
+      });
+
+      window.FB.AppEvents.logPageView();
+    };
+
+    // Load Facebook SDK asynchronously
+    (function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) { return; }
+      js = d.createElement(s); js.id = id;
+      js.src = "https://connect.facebook.net/en_US/sdk.js";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+  }, []);
 
   return (
     <div>

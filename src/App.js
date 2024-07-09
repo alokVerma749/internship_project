17a145647f03fd18366b79d817cc5acd226ba7ba
alokverma749 <alokverma749@gmail.com>
2024-07-09 20:31:53 +0530

    fix

--- src/App.js
+++ src/App.js
@@ -9,29 +9,6 @@ function App() {
     console.log(status, '###')
   }
 
-  useEffect(() => {
-    // Initialize Facebook SDK
-    window.fbAsyncInit = function() {
-      window.FB.init({
-        appId      : '762762392464716',
-        cookie     : true,
-        xfbml      : true,
-        version    : 'v20.0'
-      });
-
-      window.FB.AppEvents.logPageView();
-    };
-
-    // Load Facebook SDK asynchronously
-    (function(d, s, id) {
-      var js, fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) { return; }
-      js = d.createElement(s); js.id = id;
-      js.src = "https://connect.facebook.net/en_US/sdk.js";
-      fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
-  }, []);
-
   return (
     <div>
       <h2>Login Page</h2>
